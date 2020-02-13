5a616344af27faf91ab7c7b4128bf68733fcd2dc
Cyclic3 <cyclic3.git@gmail.com>
2020-02-13 19:18:21 +0000

    Ready for deploy

--- srv.cpp
+++ srv.cpp
@@ -10,17 +10,13 @@ int main(int argc, char** argv) {
   std::vector<std::pair<std::string, std::string>> msgs;
 
   d.recv.connect([&](const dnspp::request& req) -> std::vector<dnspp::response> {
-    if (req.name.size() == 0 || req.name.back() != "dnschat")
+    auto iter = std::find(req.name.begin(), req.name.end(), "dnschat");
+    std::vector<std::string> new_name{req.name.begin(), iter};
+    if (new_name.size() == 0)
       return {};
+    std::vector<std::string> path{iter, req.name.end()};
 
-    std::vector<dnspp::response> ret {
-      {
-        .name = {"dnschat"},
-        .value = dnspp::response::ns {
-          .nsdname = {"dnschat"}
-        }
-      }
-    };
+    std::vector<dnspp::response> ret;
 
     if (req.name.size() == 1) {
       ret.emplace_back(dnspp::response {
@@ -29,24 +25,26 @@ int main(int argc, char** argv) {
                                                          "Welcome to dnschat!",
                                                          "",
                                                          "To get started, use one of the following commands:"
+                                                         ""
                                                          "nslookup -type=txt poll.dnschat dns.c3murk.dev"
-                                                         "dig txt poll.dnschat @dns.c3murk.dev"
+                                                         ""
+                                                         "dig txt poll.dnschat.<dns>"
                                                          "",
-                                                         "Post using the domain <msg>.<uname>.dnschat"
+                                                         "Post using the domain <msg>.<uname>.dnschat.<dns>"
                                                          "",
                                                          "Have fun!"
                                                        }}
                        });
     }
-    else if (req.name.size() == 2 && req.name.at(0) == "poll") {
+    else if (new_name[0] == "poll") {
       for (auto& i : msgs)
         ret.emplace_back(dnspp::response {
-                           .name = {i.first, "dnschat"},
+                           .name = {i.first},
                            .value=dnspp::response::txt{.records={i.second}}
                          });
     }
     else {
-      msgs.emplace_back(req.name.at(1), req.name.at(0));
+      msgs.emplace_back(req.name.at(1), new_name[0]);
       ret.emplace_back(dnspp::response {
                          .name = req.name,
                          .value = dnspp::response::txt{.records={"Success"}}
