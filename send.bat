21c1a54df8c44d5f5aa3e06dc991c9610cd565f0
michalani <27767884+michalani@users.noreply.github.com>
2020-02-14 13:44:35 +0000

    Update send.bat

--- send.bat
+++ send.bat
@@ -1,11 +1,12 @@
-:top
+cls
 if not exist "%userprofile%\.dnschat_counter" (
-  echo 69%random%%random%%random%  > "%userprofile%\.dnschat_counter"
+  echo %random%%random%  > "%userprofile%\.dnschat_counter"
   REM not cryptographic, but we need no leading 0
 )
 
 set /p dnschat_counter=<%userprofile%\.dnschat_counter
-set /a dnschat_counter=%dnschat_counter + 1
+set /a dnschat_counter=%dnschat_counter% + 1
 
-nslookup -type=txt "%2".%1.%dnschat_counter.dnschat dns.c3murk.dev
-goto top
+nslookup -type=txt %2.%1.%dnschat_counter%.dnschat dns.c3murk.dev
+
+echo %dnschat_counter% > "%userprofile%\.dnschat_counter"
