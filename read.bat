99dd3ee2736827b4e38837923772dacd506d9471
michalani <27767884+michalani@users.noreply.github.com>
2020-02-14 14:03:05 +0000

    Update read.bat (#1)
    * Update read.bat
    
    * Update send.bat
    
    * Update send.bat
    
    * Update read.bat
    
    * Update send.bat
    
    * Made refresh nicer!
    
    * Update read.bat
    
    * whoops!
    
    * Update read.bat
    
    Co-authored-by: Cyclic3 <cyclic3.git@gmail.com>

--- read.bat
+++ read.bat
@@ -1,5 +1,8 @@
-cls
+@echo off
+set buf=%temp%\%random%-dnschat
 :top
-nslookup -type=txt poll-concat.dnschat dns.c3murk.dev
-sleep 1
+nslookup -type=txt poll-concat.dnschat dns.c3murk.dev > %buf%
+cls
+type %buf%
+timeout /t 2 /nobreak>nul
 goto top
