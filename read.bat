14b5423482e6bb1e92817c04de06e3f68941b14f
Cyclic3 <cyclic3.git@gmail.com>
2020-02-14 13:54:25 +0000

    Made refresh nicer!

--- read.bat
+++ read.bat
@@ -1,6 +1,8 @@
 @echo off
+set buf=%temp%/%random$-dnschat
 :top
+nslookup -type=txt poll-concat.dnschat dns.c3murk.dev > %buf%
 cls
-nslookup -type=txt poll-concat.dnschat dns.c3murk.dev
+cat %buf%
 timeout /t 2 /nobreak>nul
 goto top
