c662c39dc2b32339a3757a3b1c3186a08931c432
Cyclic3 <cyclic3.git@gmail.com>
2020-02-14 10:34:52 +0000

    dns chat now works in safe mode!

--- client.sh
+++ client.sh
@@ -1,18 +1,33 @@
 export DNSCHAT_COUNTER=$(cat ~/.dnschat_counter)
 export DNSCHAT_USER=$1
 
+export DNSCHAT_MODE=$2
+
+if [ -z $DNSCHAT_MODE ]
+then
+  export DNSCHAT_MODE=normal
+fi
+
 dnschat_send() {
   export DNSCHAT_COUNTER=$(($DNSCHAT_COUNTER + 1))
-  dig txt +noidnin +noidnout "$@".$DNSCHAT_USER.$DNSCHAT_COUNTER.dnschat @dns.c3murk.dev
+  case $DNSCHAT_MODE in
+    safe) dig txt +noidnin +noidnout "$@".$DNSCHAT_USER.$DNSCHAT_COUNTER.dnschat.foo.gifter.cf ;;
+    *) dig txt +noidnin +noidnout "$@".$DNSCHAT_USER.$DNSCHAT_COUNTER.dnschat @dns.c3murk.dev ;;
+  esac
   echo $DNSCHAT_COUNTER > ~/.dnschat_counter
 }
 
+case $DNSCHAT_MODE in
+  safe) export DNSCHAT_POLL="sh poll-safe.sh" ;;
+  *) export DNSCHAT_POLL="sh poll.sh" ;;
+esac
+
 dnschat_prompt() {
   clear
-  sh poll.sh
+  $DNSCHAT_POLL
   echo -n "dnschat> "
   read input
-  if [ "$input" = "/exit" ]
+  if [ "$input" = ".exit" ]
   then
      exit 0;
   fi
@@ -23,5 +38,5 @@ trap dnschat_prompt INT
 
 while(true)
 do
-  watch -t -n 1 "sh poll.sh"
+  watch -t -n 1 $DNSCHAT_POLL
 done
---
+++ poll-safe.sh
@@ -0,0 +1,4 @@
+dig txt +noquestion poll-concat.dnschat.foo.gifter.cf | \
+grep -v '^;.*' | \
+sed 's/.*TXT\t//g' | \
+sed -E 's/ *"(([^"]|\\")*)+"/\1\n/g'
